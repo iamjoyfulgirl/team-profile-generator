283769e41e8c6ba2e4845cff7faa195c99d31892
iamjoyfulgirl <sherri@nymeria.lan>
2022-08-21 11:10:46 -0700

    Updates index.js with requires and output path

---
+++ index.js
@@ -0,0 +1,15 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const path = require('path');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const OUTPUT_DIR = path.resolve(__dirname, 'dist');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const generateTeam = require('./src/template.js');
+
+teamArray[];
+
+
+
+
